059b3f03c55ddd4b60c60f18b1b28cb6fd1c00eb
lyc <1772653245@qq.com>
2024-06-01 22:56:55 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/wego-web/' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? './wego-web/' : './',
+    publicPath: './',
     lintOnSave: false,
     devServer: {
       port: 4002,
